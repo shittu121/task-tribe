38c842e70e1365a7f1fecba359faa92db23a9f1f
shittu121 <shittufaruq6@gmail.com>
2025-04-03 11:25:24 +0100

    Updates

--- app/.well-known/farcaster.json/route.ts
+++ app/.well-known/farcaster.json/route.ts
@@ -1,24 +1,22 @@
 export async function GET() {
-  const URL = process.env.NEXT_PUBLIC_URL;
+  const URL = process.env.NEXT_PUBLIC_URL || "https://task-tribe-liav.vercel.app";
 
   return Response.json({
     accountAssociation: {
-      header: process.env.FARCASTER_HEADER,
-      payload: process.env.FARCASTER_PAYLOAD,
-      signature: process.env.FARCASTER_SIGNATURE,
+      header: process.env.FARCASTER_HEADER || "eyJmaWQiOjk2NzQyMSwidHlwZ...",
+      payload: process.env.FARCASTER_PAYLOAD || "eyJkb21haW4iOiJ0YXNrLXRyaW...",
+      signature: process.env.FARCASTER_SIGNATURE || "MHg4ZGNiNGNlZTQ5MjNkNjViO...",
     },
     frame: {
-      version: process.env.NEXT_PUBLIC_VERSION,
-      name: process.env.NEXT_PUBLIC_ONCHAINKIT_PROJECT_NAME,
+      version: process.env.NEXT_PUBLIC_VERSION || "next",
+      name: process.env.NEXT_PUBLIC_ONCHAINKIT_PROJECT_NAME || "task-tribe",
       homeUrl: URL,
-      iconUrl: process.env.NEXT_PUBLIC_ICON_URL,
-      imageUrl: process.env.NEXT_PUBLIC_IMAGE_URL,
-      buttonTitle: `Launch ${process.env.NEXT_PUBLIC_ONCHAINKIT_PROJECT_NAME}`,
-      splashImageUrl: process.env.NEXT_PUBLIC_SPLASH_IMAGE_URL,
-      splashBackgroundColor: `#${process.env.NEXT_PUBLIC_SPLASH_BACKGROUND_COLOR}`,
+      iconUrl: process.env.NEXT_PUBLIC_ICON_URL || `${URL}/snake.png`,
+      imageUrl: process.env.NEXT_PUBLIC_IMAGE_URL || `${URL}/snake.png`,
+      buttonTitle: `Launch ${process.env.NEXT_PUBLIC_ONCHAINKIT_PROJECT_NAME || "task-tribe"}`,
+      splashImageUrl: process.env.NEXT_PUBLIC_SPLASH_IMAGE_URL || `${URL}/snake.png`,
+      splashBackgroundColor: `#${process.env.NEXT_PUBLIC_SPLASH_BACKGROUND_COLOR || "FFFFFF"}`,
       webhookUrl: `${URL}/api/webhook`,
     },
   });
 }
-
-
